74c1b2d902406bad7c0b96cfb2d97a0b53049e54
Sriram Murali <75327547+sriram1105-m@users.noreply.github.com>
2025-07-11 22:41:55 +0530

    Update bronze_raw_sales_feed.sql

--- bronze/bronze_raw_sales_feed.sql
+++ bronze/bronze_raw_sales_feed.sql
@@ -1,2 +1,9 @@
 -- SQL to load raw sales feed into bronze layer
  
+CREATE SCHEMA IF NOT EXISTS tp_bronze;
+CREATE SCHEMA IF NOT EXISTS tp_silver;
+CREATE SCHEMA IF NOT EXISTS tp_silver_validated;
+CREATE SCHEMA IF NOT EXISTS tp_gold;
+
+ALTER TABLE default.bronze_raw_sales_feed
+  RENAME TO tp_bronze.bronze_raw_sales_feed;
